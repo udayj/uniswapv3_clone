c05138073eec07643850da64d7bfffbadfa98481
udayj <jhunjhunwala.uday@gmail.com>
2024-09-20 12:44:04 +0000

    swap math

--- src/UniswapV3Pool.sol
+++ src/UniswapV3Pool.sol
@@ -5,6 +5,7 @@ import "./lib/Position.sol";
 import "./lib/TickBitmap.sol";
 import "./lib/TickMath.sol";
 import "./lib/Math.sol";
+import "./lib/SwapMath.sol";
 import "./interfaces/IUniswapV3MintCallback.sol";
 import "./interfaces/IUniswapV3SwapCallback.sol";
 import "./interfaces/IERC20.sol";
@@ -55,6 +56,23 @@ contract UniswapV3Pool {
         address token0;
         address token1;
         address payer;
+    }
+
+    struct SwapState {
+
+        uint256 amountSpecifiedRemaining;
+        uint256 amountCalculated;
+        uint160 sqrtPriceX96;
+        int24 tick;
+    }
+
+    struct StepState {
+
+        uint160 sqrtPriceStartX96;
+        int24 nextTick;
+        uint160 sqrtPriceNextX96;
+        uint256 amountIn;
+        uint256 amountOut;
     }
 
     Slot0 public slot0;
@@ -148,28 +166,86 @@ contract UniswapV3Pool {
         );
     }
 
-    function swap(address recipient, bytes calldata data) 
+    function swap(
+        address recipient,
+        bool zeroForOne,
+        uint256 amountSpecified,
+        bytes calldata data) 
         public 
         returns (int256 amount0, int256 amount1) {
 
-        int24 nextTick = 85184;
-        uint160 nextPrice = 5604469350942327889444743441197;
+       Slot0 memory slot0_ = slot0;
 
-        amount0 = -0.008396714242162444 ether;
-        amount1 = 42 ether;
+       SwapState memory state = SwapState({
+            amountSpecifiedRemaining: amountSpecified,
+            amountCalculated: 0,
+            sqrtPriceX96: slot0_.sqrtPriceX96,
+            tick: slot0_.tick
+       });
 
-        (slot0.tick, slot0.sqrtPriceX96) = (nextTick, nextPrice);
+       while (state.amountSpecifiedRemaining > 0) {
 
-        IERC20(token0).transfer(recipient, uint256(-amount0));
+        StepState memory step;
 
-        uint256 balance1Before = balance1();
-        IUniswapV3SwapCallback(msg.sender).uniswapV3SwapCallback(
-            amount0,
-            amount1,
-            data
-        );
-        if (balance1Before + uint256(amount1) > balance1())
-            revert InsufficientInputAmount();
+        step.sqrtPriceStartX96 = state.sqrtPriceX96;
+        (step.nextTick,) = tickBitmap.nextInitializedTickWithinOneWord(
+            state.tick,
+            1,
+            zeroForOne
+            );
+        
+        step.sqrtPriceNextX96 = TickMath.getSqrtRatioAtTick(step.nextTick);
+
+        (state.sqrtPriceX96, step.amountIn, step.amountOut) = SwapMath
+            .computeSwapStep(
+                step.sqrtPriceStartX96,
+                step.sqrtPriceNextX96,
+                liquidity,
+                state.amountSpecifiedRemaining
+            );
+        
+        state.amountSpecifiedRemaining -= step.amountIn;
+        state.amountCalculated += step.amountOut;
+        state.tick = TickMath.getTickAtSqrtRatio(state.sqrtPriceX96);
+       }
+
+       if (state.tick != slot0_.tick) {
+        (slot0.sqrtPriceX96, slot0.tick) = (state.sqrtPriceX96, state.tick);
+       }
+
+       (amount0, amount1) = zeroForOne
+            ? (
+                int256(amountSpecified - state.amountSpecifiedRemaining),
+                -int256(state.amountCalculated)
+            )
+            :
+            (
+                -int256(state.amountCalculated),
+                int256(amountSpecified - state.amountSpecifiedRemaining)
+            );
+        
+        if(zeroForOne) {
+
+            IERC20(token1).transfer(recipient, uint256(-amount1));
+            uint256 balance0Before = balance0();
+            IUniswapV3SwapCallback(msg.sender).uniswapV3SwapCallback(amount0, amount1, data);
+            if (balance0Before + uint256(amount0) > balance0() )
+                revert InsufficientInputAmount();
+        }
+        else {
+
+            IERC20(token0).transfer(recipient, uint256(-amount0));
+
+            uint256 balance1Before = balance1();
+            IUniswapV3SwapCallback(msg.sender).uniswapV3SwapCallback(
+                amount0,
+                amount1,
+                data
+            );
+            if (balance1Before + uint256(amount1) > balance1())
+                revert InsufficientInputAmount();
+
+        }
 
         emit Swap(
             msg.sender,
--- src/lib/Math.sol
+++ src/lib/Math.sol
@@ -15,13 +15,12 @@ library Math {
         if (sqrtPriceAX96 > sqrtPriceBX96)
             (sqrtPriceAX96, sqrtPriceBX96) = (sqrtPriceBX96, sqrtPriceAX96);
         require(sqrtPriceAX96>0);
-        amount0 = PRBMath.mulDiv(
-            PRBMath.mulDiv(
+        amount0 = divRoundingUp(
+            mulDivRoundingUp(
                 (uint256(liquidity) << FixedPoint96.RESOLUTION),
-                sqrtPriceBX96-sqrtPriceAX96,
+                (sqrtPriceBX96 - sqrtPriceAX96),
                 sqrtPriceBX96
             ),
-            1,
             sqrtPriceAX96
         );
     }
@@ -35,10 +34,92 @@ library Math {
         if (sqrtPriceAX96 > sqrtPriceBX96)
             (sqrtPriceAX96, sqrtPriceBX96) = (sqrtPriceBX96, sqrtPriceAX96);
         
-        amount1 = PRBMath.mulDiv(
-            (uint256(liquidity) << FixedPoint96.RESOLUTION),
+        amount1 = mulDivRoundingUp(
+            liquidity,
             (sqrtPriceBX96 - sqrtPriceAX96),
             FixedPoint96.Q96
         );
     }
+
+    function getNextSqrtPriceFromInput(
+        uint160 sqrtPriceX96,
+        uint128 liquidity,
+        uint256 amountIn,
+        bool zeroForOne
+    ) internal pure returns (uint160 sqrtPriceNextX96) {
+
+        sqrtPriceNextX96 = zeroForOne
+            ? getNextSqrtPriceFromAmount0RoundingUp(
+                sqrtPriceX96,
+                liquidity,
+                amountIn
+            )
+            : getNextSqrtPriceFromAmount1RoundingDown(
+                sqrtPriceX96,
+                liquidity,
+                amountIn
+            );
+    }
+
+    function getNextSqrtPriceFromAmount0RoundingUp(
+        uint160 sqrtPriceX96,
+        uint128 liquidity,
+        uint256 amountIn
+    ) internal pure returns (uint160) {
+        uint256 numerator = uint256(liquidity) << FixedPoint96.RESOLUTION;
+        uint256 product = amountIn * sqrtPriceX96;
+
+        // If product doesn't overflow, use the precise formula.
+        if (product / amountIn == sqrtPriceX96) {
+            uint256 denominator = numerator + product;
+            if (denominator >= numerator) {
+                return
+                    uint160(
+                        mulDivRoundingUp(numerator, sqrtPriceX96, denominator)
+                    );
+            }
+        }
+
+        // If product overflows, use a less precise formula.
+        return
+            uint160(
+                divRoundingUp(numerator, (numerator / sqrtPriceX96) + amountIn)
+            );
+    }
+
+    function getNextSqrtPriceFromAmount1RoundingDown(
+        uint160 sqrtPriceX96,
+        uint128 liquidity,
+        uint256 amountIn
+    ) internal pure returns (uint160) {
+        return
+            sqrtPriceX96 +
+            uint160((amountIn << FixedPoint96.RESOLUTION) / liquidity);
+    }
+
+    function mulDivRoundingUp(
+        uint256 a,
+        uint256 b,
+        uint256 denominator
+    ) internal pure returns (uint256 result) {
+        result = PRBMath.mulDiv(a, b, denominator);
+        if (mulmod(a, b, denominator) > 0) {
+            require(result < type(uint256).max);
+            result++;
+        }
+    }
+
+    function divRoundingUp(uint256 numerator, uint256 denominator)
+        internal
+        pure
+        returns (uint256 result)
+    {
+        assembly {
+            result := add(
+                div(numerator, denominator),
+                gt(mod(numerator, denominator), 0)
+            )
+        }
+    }
+
 }
