477dce59532f1de84f67d138f006536980052c62
udayj <jhunjhunwala.uday@gmail.com>
2024-09-26 16:29:37 +0000

    tick math

--- src/lib/Tick.sol
+++ src/lib/Tick.sol
@@ -49,12 +49,73 @@ library Tick {
 
     }
 
-     function cross(mapping(int24 => Tick.Info) storage self, int24 tick)
+     function cross(
+        mapping(int24 => Tick.Info) storage self, 
+        int24 tick,
+        uint256 feeGrowthGlobal0X128,
+        uint256 feeGrowthGlobal1X128)
         internal
-        view
         returns (int128 liquidityDelta)
     {
         Tick.Info storage info = self[tick];
+
+        info.feeGrowthOutside0X128 = feeGrowthGlobal0X128 - info.feeGrowthOutside0X128;
+        info.feeGrowthOutside1X128 = feeGrowthGlobal1X128 - info.feeGrowthOutside1X128;
         liquidityDelta = info.liquidityNet;
+    }
+
+    function getFeeGrowthInside(
+        mapping(int24 => Tick.Info) storage self,
+        int24 lowerTick_,
+        int24 upperTick_,
+        int24 currentTick,
+        uint256 feeGrowthGlobal0X128,
+        uint256 feeGrowthGlobal1X128
+    )
+    internal
+    view
+    returns (uint256 feeGrowthInside0X128, uint256 feeGrowthInside1X128) {
+
+        Tick.Info storage lowerTick = self[lowerTick_];
+        Tick.Info storage upperTick = self[upperTick_];
+
+        uint256 feeGrowthBelow0X128;
+        uint256 feeGrowthBelow1X128;
+
+        if (currentTick >= lowerTick_) {
+            feeGrowthBelow0X128 = lowerTick.feeGrowthOutside0X128;
+            feeGrowthBelow1X128 = lowerTick.feeGrowthOutside1X128;
+        } else {
+            feeGrowthBelow0X128 =
+                feeGrowthGlobal0X128 -
+                lowerTick.feeGrowthOutside0X128;
+            feeGrowthBelow1X128 =
+                feeGrowthGlobal1X128 -
+                lowerTick.feeGrowthOutside1X128;
+        }
+
+        uint256 feeGrowthAbove0X128;
+        uint256 feeGrowthAbove1X128;
+        if (currentTick < upperTick_) {
+            feeGrowthAbove0X128 = upperTick.feeGrowthOutside0X128;
+            feeGrowthAbove1X128 = upperTick.feeGrowthOutside1X128;
+        } else {
+            feeGrowthAbove0X128 =
+                feeGrowthGlobal0X128 -
+                upperTick.feeGrowthOutside0X128;
+            feeGrowthAbove1X128 =
+                feeGrowthGlobal1X128 -
+                upperTick.feeGrowthOutside1X128;
+        }
+
+        feeGrowthInside0X128 =
+            feeGrowthGlobal0X128 -
+            feeGrowthBelow0X128 -
+            feeGrowthAbove0X128;
+        feeGrowthInside1X128 =
+            feeGrowthGlobal1X128 -
+            feeGrowthBelow1X128 -
+            feeGrowthAbove1X128;
+
     }
 }
